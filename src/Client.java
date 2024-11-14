bc55729f85b57166c46c0ee65918a9bfcf45e8c7
edouardmaz <edouard.mazars@outlook.fr>
2024-11-14 09:17:00 +0100

    test

---
+++ src/Client.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+
+	public static void main(String[] args) throws IOException {
+		
+		Socket echoSocket = null;
+		PrintWriter out = null;
+		BufferedReader in = null;
+		try {
+			echoSocket=new Socket("Ordenateur",4444); //à modifier
+			out=new PrintWriter(echoSocket.getOutputStream(),true);
+			in=new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+		}catch(UnknownHostException e) {
+			System.out.println("perdu");
+			System.exit(-1);
+		}catch(IOException e) {
+			System.out.println("io error");
+			System.exit(-1);
+		}
+		BufferedReader mess=new BufferedReader(new InputStreamReader(System.in));
+		String userinput;
+		while((userinput=mess.readLine()).equals("bye")==false) {
+			System.out.println(userinput!="bye");
+			out.println(userinput);
+			System.out.println("echo:"+in.readLine());
+			System.out.println("meta:"+userinput);
+			
+		}
+		System.out.println("fin");
+		out.close();
+		in.close();
+		mess.close();
+		echoSocket.close();
+	}
+}
---
+++ src/ResolutionDeNom.java
@@ -0,0 +1,16 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class ResolutionDeNom {
+	public static void main(String[] args) {
+		InetAddress address;
+		try {
+			String host="Ordenateur";
+			address=InetAddress.getByName(host);
+			System.out.println(host+":"+address.getHostAddress());
+		}catch(UnknownHostException e) {
+			e.printStackTrace();
+		}
+	}
+
+}

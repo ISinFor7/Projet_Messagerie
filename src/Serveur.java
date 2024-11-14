daf0d97a097eace9a0d0dbc8b1f329ab0eec4609
ISinFor7 <yahia.berrouiguet.lilo@gmail.com>
2024-11-14 11:20:06 +0100

    Changement du serveur en un serveur utilisant des threads (non testé)

--- src/Serveur.java
+++ src/Serveur.java
@@ -7,12 +7,57 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Serveur {
+public class Serveur extends Thread {
+	ServerSocket serverSocket=null;//créé le socket afin de recevoir les connections sur le serveur
+	private static ExecutorService executor = Executors.newFixedThreadPool(100);//met une limite pour la création de Threads
+
+	Serveur() {
+		try {
+			serverSocket=new ServerSocket(4444);//mise en place du socket sur le port 4444
+			this.start();//lancement du thread principal
+		}catch (IOException e) {
+			System.out.println("erreur port 4444");
+			System.exit(-1);
+		}
+	}
+	
+	public void run() {
+		while (true) {
+			try {
+				
+				final Socket clientSocket=serverSocket.accept();
+				executor.execute(new Runnable() {//lancement d'un thread pour un client
+					public void run() { 
+						try {
+							//clientSocket=serverSocket.accept();
+							out=new PrintWriter(clientSocket.getOutputStream(),true);
+							in=new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+							BufferedReader messer=new BufferedReader(new InputStreamReader(System.in));
+							String hostinput;
+							while ((hostinput=messer.readLine()).equals("bye")==false) {
+								out.println(hostinput);
+								System.out.println(hostinput);
+								System.out.println("user:"+in.readLine());
+								System.out.println("meta"+hostinput);
+							}
+						}catch(IOException e) {
+							System.out.println("echec port 4444");
+							System.exit(-1);
+						}
+					}
+				})
+			} catch (IOException ex) {
+				Logger.getLogger(SimpleServer.class.getName()).log(Level.SEVERE, null, ex);
+			} catch (Exception e) {
+				System.out.println("Exceptiopn: "+e);
+			}
+		}
+	}
 	
 	public static void main(String[] args) throws IOException {
+		Serveur s = new Serveur()
 		
-		ServerSocket serverSocket=null;
-		try {
+		/* try {
 			serverSocket=new ServerSocket(4444);
 		}catch (IOException e) {
 			System.out.println("erreur port 4444");
@@ -43,6 +88,6 @@ public class Serveur {
 		in.close();
 		messer.close();
 		clientSocket.close();
-	}
+	}*/
 
 }

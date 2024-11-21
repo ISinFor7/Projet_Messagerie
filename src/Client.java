c3eae76a05cdf171b252135b1cdbc55ebdb564d1
edouardmaz <edouard.mazars@outlook.fr>
2024-11-21 08:40:07 +0100

    amelioration client-serveur

--- src/Client.java
+++ src/Client.java
@@ -4,33 +4,41 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.security.Key;
+
+import javax.crypto.Cipher;
 
 public class Client {
 
 	public static void main(String[] args) throws IOException {
-		
+
 		Socket echoSocket = null;
 		PrintWriter out = null;
 		BufferedReader in = null;
+		byte[] data;
+		byte[] result;
+		byte[] original;
+		Key cle=null;
+		Cipher chiffrem=null;
 		try {
-			echoSocket=new Socket("Ordenateur",4444); //à modifier
-			out=new PrintWriter(echoSocket.getOutputStream(),true);
-			in=new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
-		}catch(UnknownHostException e) {
+			echoSocket = new Socket("Ordenateur", 4444); // à modifier
+			out = new PrintWriter(echoSocket.getOutputStream(), true);
+			in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+			System.out.println("connécté");
+		} catch (UnknownHostException e) {
 			System.out.println("perdu");
 			System.exit(-1);
-		}catch(IOException e) {
+		} catch (IOException e) {
 			System.out.println("io error");
 			System.exit(-1);
 		}
-		BufferedReader mess=new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader mess = new BufferedReader(new InputStreamReader(System.in));
 		String userinput;
-		while((userinput=mess.readLine()).equals("bye")==false) {
-			System.out.println(userinput!="bye");
+		while ((userinput = mess.readLine()).equals("bye") == false) {
 			out.println(userinput);
-			System.out.println("echo:"+in.readLine());
-			System.out.println("meta:"+userinput);
-			
+			System.out.println("echo:" + in.readLine());
+			System.out.println("meta:" + userinput);
+
 		}
 		System.out.println("fin");
 		out.close();
